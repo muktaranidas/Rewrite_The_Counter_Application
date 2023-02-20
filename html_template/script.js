3f15e9230143436073e8006ee1cede442f702214
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-02-19 19:05:42 -0800

    complete 1.10

--- html_template/script.js
+++ html_template/script.js
@@ -1,37 +1,93 @@
-// select dom elements
-const counterEl = document.getElementById("counter");
-const incrementEl = document.getElementById("increment");
-const decrementEl = document.getElementById("decrement");
+// // select dom elements
+// const counterEl = document.getElementById("counter");
+// const incrementEl = document.getElementById("increment");
+// const decrementEl = document.getElementById("decrement");
 
-// action identifiers
-const INCREMENT = "increment";
-const DECREMENT = "decrement";
+// // action identifiers
+// const INCREMENT = "increment";
+// const DECREMENT = "decrement";
 
-// action creators
-const increment = (value) => {
-  return {
-    type: INCREMENT,
-    payload: value,
-  };
-};
-const decrement = (value) => {
-  return {
-    type: DECREMENT,
-    payload: value,
-  };
-};
+// // action creators
+// const increment = (value) => {
+//   return {
+//     type: INCREMENT,
+//     payload: value,
+//   };
+// };
+// const decrement = (value) => {
+//   return {
+//     type: DECREMENT,
+//     payload: value,
+//   };
+// };
 
-// initial state
+// // initial state
+// const initialState = {
+//   value: 0,
+//   properties: {
+//     a: 5,
+//     b: 6,
+//   },
+// };
+
+// // create reducer function
+// function counterReducer(state = initialState, action) {
+//   if (action.type === "increment") {
+//     return {
+//       ...state,
+//       value: state.value + action.payload,
+//     };
+//   } else if (action.type === "decrement") {
+//     return {
+//       ...state,
+//       value: state.value - action.payload,
+//     };
+//   } else if (action.type === "ITEST") {
+//     const updatedState = {
+//       ...state,
+//       properties: {
+//         ...state.properties,
+//         b: state.properties.b + 1,
+//       },
+//     };
+//     return updatedState;
+//   } else {
+//     return state;
+//   }
+// }
+
+// // create store
+// const store = Redux.createStore(counterReducer);
+
+// const render = () => {
+//   const state = store.getState();
+//   counterEl.innerText = state.value.toString();
+// };
+
+// //update UI initially
+// render();
+
+// store.subscribe(render);
+
+// // button click listeners
+// incrementEl.addEventListener("click", () => {
+//   store.dispatch(increment(3));
+// });
+// decrementEl.addEventListener("click", () => {
+//   store.dispatch(decrement(2));
+// });
+
+// module 1.10
+const actions = [
+  { type: "increment", payload: 1 },
+  { type: "increment", payload: 1 },
+  { type: "increment", payload: 1 },
+  { type: "decrement", payload: 1 },
+];
 const initialState = {
   value: 0,
-  properties: {
-    a: 5,
-    b: 6,
-  },
 };
-
-// create reducer function
-function counterReducer(state = initialState, action) {
+const counterReducer = (state, action) => {
   if (action.type === "increment") {
     return {
       ...state,
@@ -42,37 +98,10 @@ function counterReducer(state = initialState, action) {
       ...state,
       value: state.value - action.payload,
     };
-  } else if (action.type === "ITEST") {
-    const updatedState = {
-      ...state,
-      properties: {
-        ...state.properties,
-        b: state.properties.b + 1,
-      },
-    };
-    return updatedState;
   } else {
     return state;
   }
-}
-
-// create store
-const store = Redux.createStore(counterReducer);
-
-const render = () => {
-  const state = store.getState();
-  counterEl.innerText = state.value.toString();
 };
 
-//update UI initially
-render();
-
-store.subscribe(render);
-
-// button click listeners
-incrementEl.addEventListener("click", () => {
-  store.dispatch(increment(3));
-});
-decrementEl.addEventListener("click", () => {
-  store.dispatch(decrement(2));
-});
+const finalResult = actions.reduce(counterReducer, initialState);
+console.log(finalResult);
