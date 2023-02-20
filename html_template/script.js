85cabc75100232e50f56f272a35df5e84da91ad6
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2023-02-19 16:08:22 -0800

    complete 1.7

--- html_template/script.js
+++ html_template/script.js
@@ -3,6 +3,24 @@ const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+// action identifiers
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+// action creators
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value,
+  };
+};
+
 // initial state
 const initialState = {
   value: 0,
@@ -13,12 +31,12 @@ function counterReducer(state = initialState, action) {
   if (action.type === "increment") {
     return {
       ...state,
-      value: state.value + 1,
+      value: state.value + action.payload,
     };
   } else if (action.type === "decrement") {
     return {
       ...state,
-      value: state.value - 1,
+      value: state.value - action.payload,
     };
   } else {
     return state;
@@ -40,12 +58,8 @@ store.subscribe(render);
 
 // button click listeners
 incrementEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "increment",
-  });
+  store.dispatch(increment(3));
 });
 decrementEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "decrement",
-  });
+  store.dispatch(decrement(2));
 });
